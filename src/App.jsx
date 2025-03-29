6df50dd11cd0a1c1350e3559e1205fdb1a2c31e2
Raz <panhawath18@gmail.com>
2025-03-29 23:45:47 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className="bg-red-500">
+        <h1>app</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
