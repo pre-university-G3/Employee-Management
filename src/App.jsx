f94b84013f79c523386a2de1c53f0d81898091dc
Raz <panhawath18@gmail.com>
2025-03-29 23:54:50 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <div className="bg-red-500">
-        <h1>app</h1>
+      <div className="bg-black text-white text-[100px] text-center font-bold h-[100vh] content-center">
+        <h1>MIX DELETE GITHUB JERNG SOKLENG ? </h1>
       </div>
     </main>
   )
