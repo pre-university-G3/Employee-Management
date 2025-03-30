cf74ec8e2f5ab9b87f6bb1153b4b4b3b9bbf6e6f
Raz <panhawath18@gmail.com>
2025-03-30 18:39:33 +0700

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+// import Login from './components/navbar/Login'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
   return (
-    <main>
-      <div className="bg-black text-white text-[100px] text-center font-bold h-[100vh] content-center">
-        <h1>MIX DELETE GITHUB JERNG SOKLENG ? </h1>
-      </div>
-    </main>
+    <>
+    <Navbar />
+    </>
   )
 }
 
