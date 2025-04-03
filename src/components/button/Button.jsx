b4f1e939cc37467533e818380918f6be218ee470
Raz <panhawath18@gmail.com>
2025-04-03 14:57:34 +0700

    new

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <button className="bg-white text-primary-color font-semibold border-2 border-blue-950 px-6 py-3 mt-5 hover:bg-blue-950 hover:text-white transition duration-300 transform hover:scale-105">
+    <button className="bg-white text-primary-color font-semibold border-2 border-blue-950 px-6 py-3 mt-5 hover:bg-blue-950 hover:text-white transition duration-300 transform hover:scale-105 cursor-pointer">
     GET STARTED
   </button>
   )
