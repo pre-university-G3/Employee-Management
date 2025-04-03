bed3c5ca88aa42e847bad70e1b3b0c4c8647f1cb
Raz <panhawath18@gmail.com>
2025-04-03 15:00:09 +0700

    new

--- src/components/button/Submit.jsx
+++ src/components/button/Submit.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Submit = () => {
   return (
-    <button className="border-2 border-blue-950 px-6 py-3 mt-5 hover:bg-blue-950 hover:text-white transition duration-300 transform hover:scale-105">
+    <button className="border-2 border-blue-950 px-6 py-3 mt-5 hover:bg-blue-950 hover:text-white transition duration-300 transform hover:scale-105 cursor-pointer">
     Submit
   </button>
   )
