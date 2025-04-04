d49f714b90400855570f0a4c5e3b66e529ade510
Manith991 <rithsaramanith123@gmail.com>
2025-04-04 19:39:16 +0700

    Friday

--- src/components/pages/EMPContent.jsx
+++ src/components/pages/EMPContent.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import Sidebar from './SideBar';
-import DashBoard from './DashBoard';
+import React from 'react'
+import Sidebar from './SideBar'
+import Dashboard from './DashBoard'
 
 export default function EMPContent() {
   return (
-    <>
-      <div className="grid grid-cols-[420px_1fr] h-[1080px]">
-        <div className="Left h-full">
-          <Sidebar />
+    <main className='flex flex-row w-full h-screen'>
+        <div className="Left-side w-[19%]">
+            <Sidebar/>
         </div>
-        <div className="Right">
-          <DashBoard/>  
+        <div className="Right-side w-[81%]">
+            <Dashboard/>
         </div>
-      </div>
-    </>
-  );
+    </main>
+
+  )
 }
